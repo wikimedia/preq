700e48ce3f128b877497ba4fcfd5f5e05037d635
Gabriel Wicke <wicke@wikidev.net>
2014-10-01 11:58:15 -0700

    Return HTTPErrorResponse objects for errors & release 0.2.0
    This object directly wraps a response, so all response properties are directly
    available on the error object.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ if (!global.Promise) {
     global.Promise = require('bluebird');
 }
 
+var util = require('util');
+
 if (!Array.prototype.last) {
     Array.prototype.last = function() {
         return this[this.length - 1];
@@ -35,9 +37,22 @@ function getOptions(uri, o) {
     return o;
 }
 
+/*
+ * Error instance wrapping HTTP error responses
+ *
+ * Has the same properties as the original response.
+ */
+function HTTPErrorResponse(response) {
+    for (var key in response) {
+        this[key] = response[key];
+    }
+    this.stack = this.toString();
+}
+util.inherits(HTTPErrorResponse, Error);
+
 function wrap(method) {
     return function (url, options) {
-        var options = getOptions(url, options);
+        options = getOptions(url, options);
         if (method) {
             options.method = method;
         }
@@ -45,18 +60,25 @@ function wrap(method) {
             var cb = function(err, res) {
                 if (err || !res) {
                     if (!err) {
-                        err = new Error('Response error');
-                        err.response = {
+                        err = new HTTPErrorResponse({
                             status: 500,
                             body: {
                                 type: 'empty_response',
                             }
-                        };
+                        });
+                    } else {
+                        err =  new HTTPErrorResponse({
+                            status: 500,
+                            body: {
+                                type: 'internal_error'
+                            },
+                            stack: err.stack
+                        });
                     }
                     return reject(err);
                 }
-                if (res.body && res.headers
-                        && /^application\/json/.test(res.headers['content-type'])) {
+                if (res.body && res.headers &&
+                        /^application\/json/.test(res.headers['content-type'])) {
                     res.body = JSON.parse(res.body);
                 }
 
@@ -67,10 +89,7 @@ function wrap(method) {
                 };
 
                 if (ourRes.status >= 400) {
-                    var e = new Error('Response error:\n'
-                            + JSON.stringify(ourRes, null, 2));
-                    e.response = ourRes;
-                    reject(e);
+                    reject(new HTTPErrorResponse(ourRes));
                 } else {
                     resolve(ourRes);
                 }
